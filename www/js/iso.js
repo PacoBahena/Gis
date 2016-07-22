13c15648ea5c1641dbe79653008823c6cbe73f51
DeividZavala <crusedmoss@gmail.com>
2016-07-22 13:27:03 -0500

    se agrego funcion para csv

--- www/js/iso.js
+++ www/js/iso.js
@@ -31,6 +31,70 @@ function leafletExample(){
 
     // draw isodistance polygon
     var polyCoords = Array(), coord, polygon, isocoords = isoline.isocoords.split(",");
+    console.log(isocoords)
+    
+     //a partir de aquí es el código que me madaste del la página
+
+    function convertArrayofObjectsToCSV(args) {
+        var result,
+            ctr,
+            keys,
+            columnDelimiter,
+            lineDelimiter,
+            data;
+
+        data = args.data || null ;
+        if (data == null || !data.length){
+            return null
+        }
+
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
+
+        keys = Object.keys[0];
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function(item) {
+            ctr = 0;
+            keys.forEach(function(key) {
+                if (ctr > 0) result += columnDelimiter;
+
+                result += item[key];
+                ctr++
+            });
+            result += lineDelimiter;
+        });
+
+        return result;
+
+    }
+
+    function downloadCSV(args) {
+        var data,
+            filename,
+            link;
+        
+        var csv = convertArrayofObjectsToCSV({
+            data:isocoords
+        });
+
+        if( csv == null) return;
+
+        filename = args.filename || 'export.csv';
+
+        if (!csv.match(/^data:text:\/csv/i)){
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data= encodeURI(csv);
+
+        link = document.createElement('a')
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click(); 
+    }
     
     //DAVID, NO ESTOY SEGURO PERO CREO QUE LAS COORDENADAS SE GUARDAN EN ISOCOORDS
       
